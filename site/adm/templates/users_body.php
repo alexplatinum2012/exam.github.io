d6111e8b771b46e3ac9e360b06c75a40516a4388
Pestrak Aleksandr <alexplatinum2012@gmail.com>
2017-03-22 19:58:25 +0500

    H

--- site/adm/templates/users_body.php
+++ site/adm/templates/users_body.php
@@ -1,3 +1,16 @@
+<?php
+
+$pg = pg_connect("host=localhost dbname=site user=postgres password=ambassador");
+if($pg === false) echo "ERROR conect to DB";
+else echo "CONNECTION GRANTED!";
+
+pg_query($pg, "create table users (user_id serial, user_f varchar, user_i varchar, user_o varchar);");
+
+
+?>
+
+
+
 <div class="w960">
   <div class="nav">
     <?php include "templates/nav/nav.php" ?>
